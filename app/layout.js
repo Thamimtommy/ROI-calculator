e1b3ea10554d6db1009b8fcb73b2821e41315901
Thamimtommy <thamimtommybeats@gmail.com>
2024-10-03 18:20:25 +0530

    add some changes

--- app/layout.js
+++ app/layout.js
@@ -52,7 +52,7 @@ export default function RootLayout({ children }) {
         {/* Footer */}
         <footer className="bg-white border-t border-gray-200 py-8">
           <div className="container mx-auto px-4 text-center text-gray-500 text-sm">
-            <p>&copy; 2024 mathingRight, Inc. All rights reserved.</p>
+            <p>&copy; 2024 stitchflow, Inc. All rights reserved.</p>
           </div>
         </footer>
       </body>
